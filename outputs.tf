4fb3c02272899c62832d3281ab1cfde83f081893
faithful1 <faithfulanere@gmail.com>
2021-08-04 14:04:25 +0800

    add sensitive for ports

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,7 @@
 output "node-red-ip-address" {
   value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address], i.ports[*]["external"])]
   description = "The ip address of the container"
+  sensitive = true
 }
 
 output "node-red-name" {
