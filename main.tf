7513865609593c702a4cc69f8ed8fc518983169e
faithful1 <faithfulanere@gmail.com>
2021-08-05 09:00:44 +0800

    using locals to run functions in variables file

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "docker_image" "nodered_image" {
 
 # generate random string
 resource "random_string" "random" {
-  count = var.random_count
+  count = local.container_count
   length  = 4
   special = false
   upper   = false
@@ -30,12 +30,12 @@ resource "random_string" "random" {
 
 # Start a container
 resource "docker_container" "nodered_container" {
-  count = var.container_count
+  count = local.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = var.int_port
-    external = var.ext_port
+    external = var.ext_port[count.index]
   }
   volumes {
     container_path = "/data"
--- variables.tf
+++ variables.tf
@@ -1,27 +1,26 @@
 variable "ext_port" {
-  type = number
+  type = list
 
-  validation {
-    condition = var.ext_port <= 65535 && var.ext_port > 0
-    error_message = "The external port must be valid port range 0 - 65535."
-  }
+  # validation {
+  #   condition = var.ext_port <= 65535 && var.ext_port > 0
+  #   error_message = "The external port must be valid port range 0 - 65535."
+  # }
 }
 
 variable "int_port" {
   type = number
 
-  validation {
-    condition = var.int_port == 1880
-    error_message = "The internal port must be 1880."
-  }
+  # validation {
+  #   condition = var.int_port == 1880
+  #   error_message = "The internal port must be 1880."
+  # }
 }
 
-variable "container_count" {
-  type = number
-  default = 1
-}
+# variable "container_count" {
+#   type = number
+#   default = 3
+# }
 
-variable "random_count" {
-  type = number
-  default = 1
+locals {
+  container_count = length(var.ext_port) + 1
 }
